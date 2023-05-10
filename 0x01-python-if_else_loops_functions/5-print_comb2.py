815359d3ba5fcde6da26090159990b73378577bd
Frida <amina.amer.9898@gmail.com>
2023-05-09 17:57:40 -0700

    Task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# loop through the numbers from 0 to 99 and print them
+# loop through the numbers from 0 to 99 and output them
 for i in range(0, 100):
     if i != 99:
         print("{:02d}, ".format(i), end="")
