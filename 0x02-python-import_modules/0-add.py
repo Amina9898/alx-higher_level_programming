bc776fe97893262bc3ec8bfa01a67d35635fd75e
Frida <amina.amer.9898@gmail.com>
2023-05-11 15:34:27 -0700

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
+    a = 1
 
-b = 2
+    b = 2
 
-c = add(a, b)
-
-print("{} + {} = {}".format(a, b, c))
-
+    c = add(a, b)
