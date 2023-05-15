99b26afbcae417386575b533521103630bce3d86
Frida <amina.amer.9898@gmail.com>
2023-05-15 14:38:03 -0700

    Task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+
+    length = len(my_list)
+
+    if length != 0:
+
+        my_list.reverse()
+
+        for i in range(length):
+
+            print("{}".format(my_list[i]))
