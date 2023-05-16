e49576e2c72c6661c8226ecbf6c2866fd06f2f3b
Frida <amina.amer.9898@gmail.com>
2023-05-15 18:18:49 -0700

    Task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + (0, 0)[:2 - len(tuple_a)]
+    if len(tuple_b) < 2:
+        tuple_b = tuple_b + (0, 0)[:2 - len(tuple_b)]
+
+    total_a = tuple_a[0] + tuple_b[0]
+    total_b = tuple_a[1] + tuple_b[1]
+
+    return (total_a, total_b)
