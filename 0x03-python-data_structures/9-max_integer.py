c1254b8d031906823e1da16a56efa3ea1b9fa4fb
Frida <amina.amer.9898@gmail.com>
2023-05-15 19:07:11 -0700

    Task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    i = len(my_list) - 1
+    while i > 1:
+        j = 0
+        while j < i:
+
+            if my_list[j] > my_list[j+1]:
+                biggest = my_list[j]
+            else:
+                biggest = my_list[j+1]
+            j += 1
+        i -= 1
+    return biggest
