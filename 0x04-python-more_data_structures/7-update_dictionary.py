565bd49f74d337aecd52afba56a300d3b9654c8a
Frida <amina.amer.9898@gmail.com>
2023-05-17 18:43:17 -0700

    Task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for i in a_dictionary:
-        if i == key:
-            a_dictionary[key] = value
-        else:
-            a_dictionary[i] = a_dictionary[key] = value
+    a_dictionary[key] = value
