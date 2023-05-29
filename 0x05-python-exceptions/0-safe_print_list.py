16eba9de8fb9a35170e8c2872830b524f87178d8
Frida <amina.amer.9898@gmail.com>
2023-05-29 13:50:12 -0700

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     j = 0
     alist = ""
     for i in (my_list):
-        if j <= x:
+        if j <= (x - 1):
             j += 1
             try:
                 alist += i
