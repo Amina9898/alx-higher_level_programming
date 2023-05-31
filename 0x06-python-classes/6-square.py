160a0e84d8d38b9844c2b84434ecc094e5dd0839
Frida <amina.amer.9898@gmail.com>
2023-05-30 19:50:31 -0700

    Task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,14 +34,15 @@ class Square:
     def position(self, value):
         """Property setter"""
         self.value = value
-        if (not isinstance(value, tuple) or len(value) != 2 or not all (isinstance(num, int) for num in value) or not all(num >= 0 for num in value)):
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        self.__position = value
 
     def area(self):
         """public instance methed that return square area"""
-        return int(self.__size) * int(self.__size)
+        return (self.__size) * (self.__size)
 
     def my_print(self):
         """
@@ -51,9 +52,9 @@ class Square:
             print()
             return
         else:
-            for i in range(self.__position[1]):
+            for i in range(0, self.__position[1]):
                 print()
-            for i in range(self.__size):
+            for i in range(0, self.__size):
                 for j in range(self.__position[0]):
                     print(" ", end="")
                 for m in range(self.__size):
