36242e51887a87389b44bffcf61cf123366061a1
Frida <amina.amer.9898@gmail.com>
2023-05-30 17:18:14 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    '''This class represents a square onject'''
+
+    def __init__(self, size):
+        '''
+        Initialize a square object with a given size
+        '''
+        self.__size = size
