9d65023a0ccec1fc2fb3bf82303585f7562153a4
Frida <amina.amer.9898@gmail.com>
2023-06-05 19:49:24 -0700

    Puzzle

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+"""Solves the N-queens puzzle."""
+
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's."""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
