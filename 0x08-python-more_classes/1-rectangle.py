0d78ea90bb55476acdf111c223a8f72b471b6ef2
Frida <amina.amer.9898@gmail.com>
2023-06-05 19:38:40 -0700

    Task 1 fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-"""This module defines a class rectangle."""
+"""This is a module that defines a Rectangle class"""
 
 
 class Rectangle:
-    """
-    Represents a rectagle.
-    """
+    """Represents a class object."""
     def __init__(self, width=0, height=0):
-        """Initialises self method and private instance attributes
-        width and height"""
+        """
+        Initializes a new Rectangle.
+
+        Args
+            width :The width of the new rectangle
+            height: The height of the new rectangle
+        """
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        """Property to retrieve width aka getter"""
+        """width getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Property setter to set width"""
+        """width setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -28,14 +31,26 @@ class Rectangle:
 
     @property
     def height(self):
-        """Property to retrieve height aka height getter"""
+        """height getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Property setter to set height"""
+        """height setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """Method that returns area of rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Method that returns perimeter of rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            per = 0
+        else:
+            per = (self.__width + self.__height) * 2
+        return per
