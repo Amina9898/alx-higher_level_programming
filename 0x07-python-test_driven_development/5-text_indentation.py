0e19838a00b151d6f42928e9d9e984115eb66414
Frida <amina.amer.9898@gmail.com>
2023-06-09 18:19:36 -0700

    fix function

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,16 +11,19 @@ def text_indentation(text):
         text: input to iterate through and indent
     Returns: indented text
     """
-    txt = ""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
 
-    for i in text:
-        txt += i
-        if i == "." or i == "?" or i == ":":
-            print(txt.strip())
-            print()
-            print()
-            txt = ""
-
-    print(txt.strip())
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
