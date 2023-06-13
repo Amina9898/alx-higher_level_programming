6f5d8a482202c96358da73ba90ce532fbb323dbe
Frida <amina.amer.9898@gmail.com>
2023-06-12 17:51:07 -0700

    Task 4

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module represents a class"""
+
+
+class BaseGeometry:
+    """Class"""
+
+    def area(self):
+        """Public instance method that raises an Exception"""
+        raise Exception("area() is not implemented")
