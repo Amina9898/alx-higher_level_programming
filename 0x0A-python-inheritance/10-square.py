ef653bfd4698c15a9a203aca948141b2da45e2c4
Frida <amina.amer.9898@gmail.com>
2023-06-12 19:21:31 -0700

    Task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,7 +12,3 @@ class Square(BaseGeometry):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        """Returns the area of the square."""
-        return self.__size * self.__size
