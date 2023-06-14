d7a12921cde518142672745dcfb28086441e740d
Frida <amina.amer.9898@gmail.com>
2023-06-13 19:06:48 -0700

    Advanced task

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,9 +3,13 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """
+    function that inserts a line of text to a file,
+    after each line containing a specific string
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         lines = f.readlines()
-    
+
     with open(filename, 'w', encoding="utf-8") as f:
         for line in lines:
             f.write(line)
