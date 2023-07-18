18980345dc39578bf265a6bff21c26a6e6d986d0
Frida <amina.amer.9898@gmail.com>
2023-07-18 15:11:19 -0700

    Task 1

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- A script that creates the database hbtn_0c_0 in a MySQL server.
+
+-- If the database hbtn_0c_0 already exists, the script should not fail
+CREATE database if not exists hbtn_0c_0
