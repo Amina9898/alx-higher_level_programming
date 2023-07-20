f4abd93528bdd1b10c9aa0721fb06576d62f1c74
Frida <amina.amer.9898@gmail.com>
2023-07-19 17:58:14 -0700

    Task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,10 +3,10 @@
 CREATE DATABASE
 	IF NOT EXISTS hbtn_0d_2;
 CREATE USER
-	IF NOT EXISTS 'user_0d_2'@'localhost'
+	IF NOT EXISTS 'user_0d_2'@'%'
 	IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
 	ON htbn_0d_2.*
-	TO 'user_0d_2'@'localhost'
+	TO 'user_0d_2'@'%'
 	IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
