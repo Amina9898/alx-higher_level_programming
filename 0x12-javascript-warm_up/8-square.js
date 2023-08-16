8d560762805b6655fd36231081a9023dc61854f9
Frida <amina.amer.9898@gmail.com>
2023-08-16 12:54:01 -0700

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 const g = parseInt(process.argv[2]);
-for (let i = 0; i < g; i++) {
-  let line = '';
-  for (let j = 0; j < g; j++) {
-    line += 'X';
+if (!(isNaN(g))) {
+  for (let i = 0; i < g; i++) {
+    let line = '';
+    for (let j = 0; j < g; j++) {
+      line += 'X';
+    }
+    console.log(line);
   }
-  console.log(line);
 }
