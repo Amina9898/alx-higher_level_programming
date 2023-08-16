242e251046fec1d463ca0618a156ae5883c89789
Frida <amina.amer.9898@gmail.com>
2023-08-16 12:11:42 -0700

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (typeof parseInt(process.argv[2]) === 'number' && !isNaN(process.argv[2])) {
-  console.log(process.argv[2]);
+  console.log('My number: ' + process.argv[2]);
 } else {
   console.log('Not a number');
 }
