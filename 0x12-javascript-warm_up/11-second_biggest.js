ad37fdec77429cd05258094397e1a661f1b715e4
Frida <amina.amer.9898@gmail.com>
2023-08-16 13:40:31 -0700

    Tasks 11 to 13

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+function findSecondBiggest(arr) {
+  if (arr.length <= 1) {
+    return 0;
+  }
+
+  let biggest = Number.MIN_SAFE_INTEGER;
+  let secondBiggest = Number.MIN_SAFE_INTEGER;
+
+  for (const num of arr) {
+    const num1 = parseInt(num);
+    if (num1 > biggest) {
+      secondBiggest = biggest;
+      biggest = num1;
+    } else if (num1 > secondBiggest && num1 < biggest) {
+      secondBiggest = num1;
+    }
+  }
+
+  return secondBiggest;
+}
+
+const args = process.argv.slice(2);
+const result = findSecondBiggest(args);
+console.log(result);
+
