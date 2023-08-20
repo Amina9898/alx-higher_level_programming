bb3a64c971cb92dbc4e865eb61eda664d3559ec9
Frida <amina.amer.9898@gmail.com>
2023-08-19 17:21:16 -0700

    Task 2 fix

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (h <= 0 || w <= 0) {
-      return {};
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 };
