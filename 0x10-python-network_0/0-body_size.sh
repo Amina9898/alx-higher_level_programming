5a3047f794ac1fad0691b496bd1818342fee9cee
Frida <amina.amer.9898@gmail.com>
2023-08-31 13:01:16 -0700

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 #script that prints the size of the body of the response to a URL request
 
-if [ $# -ne 1 ]; then
-    exit 1
-fi
-
-url="$1"
-response=$(curl -sI "$url")
-
-content_length=$(echo "$response" | awk -F': ''/Content-Length/{print $2}' |tr -d '/r')
-echo $content_length
+curl -s "$1" | wc -c
