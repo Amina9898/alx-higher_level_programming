a2882af72804dc08283daeec1f88773e8dbc79f3
Frida <amina.amer.9898@gmail.com>
2023-09-03 13:47:58 -0700

    Task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 # A script that sends a request to URL and return body of response
-
-if [ $# -ne 1 ]; then
-    exit 1
-fi
-
-URL="$1"
-
-response=$(curl -s -w "%{http_code}" -o /dev/stdout "$URL")
-
-http_status="${response: -3}"
-
-if [ "$http_status" = "200" ]; then
-	body=$(echo "$response" | head -c -3)
-	echo "$body"
-fi
+curl -sL "$1"
