b349cbb75d6ebc0ee9941bf6d5acd2cf1cfc5e6a
Frida <amina.amer.9898@gmail.com>
2023-09-05 11:07:50 -0700

    Task one edit

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     URL = sys.argv[1]
 
     request = urllib.request.Request(URL)
-    with urllib.request.Request(URL) as response:
+    with urllib.request.urlopen(request) as response:
         print(dict(response.headers).get("X-Request-Id"))
