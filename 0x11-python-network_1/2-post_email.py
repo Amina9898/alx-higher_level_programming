d8f707c1441ae5738da9790bc7ce498f5e852cd5
Frida <amina.amer.9898@gmail.com>
2023-09-05 12:41:57 -0700

    Task  2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,15 +6,15 @@ the response (decoded in utf-8)
 """
 
 import sys
-import urllib.request 
 import urllib.parse
+import urllib.request
 
-URL = sys.argv[1]
-email = {"email": sys.argv[2]}
 
-data = urllib.parse.urlencode(email).encode("ascii")
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-request = urllib.request.Request(URL, data)
-with urllib.request.urlopen(request) as response:
-    output = response.read().decode('utf-8')
-    print(output)
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
